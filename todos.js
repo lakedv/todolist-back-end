21f7ab14d350cc46f2a378512dd98b6b85c862b0
German Mirarchi <uaterules@gmail.com>
2023-02-01 11:15:01 -0300

    put&delete

--- todos.js
+++ todos.js
@@ -31,6 +31,7 @@ router.post('/', (req, res) => {
   }
 
   //insertamos un objeto con descripción y sin "terminar"
+  /*
   db.todos.insert(todo, (err, result) => { // <-- funcion de callback 
     // generalmente, en node, los clientes de db te devuelven un callback
     // con un error (que si viene nulo quiere decirque no hubo error)
@@ -44,39 +45,40 @@ router.post('/', (req, res) => {
       todo.id = result.newId // Supongamos que así llega el nuevo id desdea db
     }
   })
+  */
 
   res.send(todo);
 })
 
 router.put('/:id', function (req, res) {
   // get item object match by `id`
-  let found = data.find(function (item) {
+  /*let found = data.find(function (item) {
     return item.id === parseInt(req.params.id);
-  });
+  });*/
+  let id = req.params.id
 
   // check if item found
-  if (found) {
+  if (id) {
     let updated = {
-      id: found.id,
       description: req.body.description, // set value of `title` get from req
       done: req.body.done // set value of `completed` get from req
     };
     // return with status 204
     // success status response code 204 indicates
     // that the request has succeeded
-    res.sendStatus(204);
-  } else {
-    res.sendStatus(404);
+    res.send(updated)
   }
 });
 
 router.delete('/:id', function (req, res) {
   // find item from array of data
-  let found = data.find(function (item) {
+  /*let found = data.find(function (item) {
     return item.id === parseInt(req.params.id);
   });
+  */
+  let id = req.params.id
 
-  if (found) {
+  /*if (id) {
     // if item found then find index at which the item is
     // stored in the `data` array
     let targetIndex = data.indexOf(found);
@@ -84,12 +86,14 @@ router.delete('/:id', function (req, res) {
     // splice means delete item from `data` array using index
     //index = conexion a dB
     data.splice(targetIndex, 1);
-  }
+  }*/
 
   // return with status 204
   // success status response code 204 indicates
   // that the request has succeeded
-  res.sendStatus(204);
+  res.json({
+      message: 'Deleted'
+  });
 });
 
 // module.exports is an object included in every JS file of Node.js

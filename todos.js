8f2583c84c85ff25c825360086ef812ba2ba5209
German Mirarchi <uaterules@gmail.com>
2023-02-10 22:15:22 -0300

    apicompleted

--- todos.js
+++ todos.js
@@ -30,77 +30,38 @@ router.get('/:id', async function (req, res) {
   res.send(todos);
 })
 
-router.post('/', (req, res) => {
-  let description = req.body.description;
+router.post('/', async (req, res) => {
+  let todos = {};
+  todos.desc = req.body.desc;
+  todos.done = false;
+  let doc = await
+    db.collection('todolist').add(todos);
+  todos.id = doc.id
 
-  let todos = {
-    id: 99,
-    description,
-    done: false
-  }
-  //insertamos un objeto con descripción y sin "terminar"
-  /*
-  db.todolist.insert(todos, (err, result) => { // <-- funcion de callback 
-    // generalmente, en node, los clientes de db te devuelven un callback
-    // con un error (que si viene nulo quiere decirque no hubo error)
-    // y el resultado de la consulta/operación
-
-    if (err) {
-      res.status(404).send({
-        error: err
-      })
-    } else {
-      todos.id = result.newId // Supongamos que así llega el nuevo id desdea db
-    }
-  })
-  */
-
-  res.send(todo);
+  console.log('Added document with ID:', doc.id)
+  res.send(todos);
 })
 
-router.put('/:id', function (req, res) {
-  // get item object match by `id`
-  /*let found = data.find(function (item) {
-    return item.id === parseInt(req.params.id);
-  });*/
+router.put('/:id', async function (req, res) {
   let id = req.params.id
-
-  // check if item found
-  if (id) {
-    let updated = {
-      description: req.body.description, // set value of `title` get from req
-      done: req.body.done // set value of `completed` get from req
-    };
-    // return with status 204
-    // success status response code 204 indicates
-    // that the request has succeeded
-    res.send(updated)
+  let todos = {};
+  let doc = await db.collection('todolist').doc(id)
+  todos.id = doc.id;
+  
+  doc.set({
+    desc: req.body.desc,
+    done: req.body.done
+  })
+    res.send(todos)
   }
-});
+);
 
-router.delete('/:id', function (req, res) {
-  // find item from array of data
-  /*let found = data.find(function (item) {
-    return item.id === parseInt(req.params.id);
-  });
-  */
-  let id = req.params.id
+router.delete('/:id', async function (req, res) {
+  let id = req.params.id;
+  await db.collection('todolist').doc(id).delete();
 
-  /*if (id) {
-    // if item found then find index at which the item is
-    // stored in the `data` array
-    let targetIndex = data.indexOf(found);
-
-    // splice means delete item from `data` array using index
-    //index = conexion a dB
-    data.splice(targetIndex, 1);
-  }*/
-
-  // return with status 204
-  // success status response code 204 indicates
-  // that the request has succeeded
   res.json({
-    message: 'Deleted'
+    message: 'Document Deleted'
   });
 });
 
